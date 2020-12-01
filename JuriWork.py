6fa870150992c089d6879699182509c4c82ebb00
Juri Ladurner <Juri.ladurner@outlook.de>
2020-12-01 11:37:54 +0100

    I wrote the comments for JuriWork.py

--- JuriWork.py
+++ JuriWork.py
@@ -2,25 +2,29 @@ import sys
 
 """
 Switch Case to open the Subprograms
-TODO; enter valid options in the cases
+Juri Ladurner
+30.11.2020
+
 """
 def switch_Menue(argument):
     switcher = {
-        1: "",
-        2: "",
+        1: "",                         # TODO; enter valid options in the cases
+        2: "",                         # TODO; enter valid options in the cases
         3: sys.exit(0)
     }
 
 
 """
-The Menue where the user can add the Subprograms
+The Menue where the user can decide with Subprograms he want to use
+Juri Ladurner
+30.11.2020
 """
 def Menue():
     possibility = 7
-    while possibility >=4 or possibility <= 0:
+    while possibility >=4 or possibility <= 0:                           # Loop if wrong input
         print("Add new password: 1\nSearch password:  2\nQuit program:     3")
         possibility = int(input("What do you want to do? "))
-    switch_Menue(possibility)
+    switch_Menue(possibility)                   # Open the "switch_Menue" function
 
 
 Menue()
