c8a564851eb4481c903c81485bd8389f7052f9c7
Siraman25 <mandoliniaris@gmail.com>
2020-12-01 12:32:20 +0100

    Updated comments for Login.py

--- Login.py
+++ Login.py
@@ -9,72 +9,88 @@ import os
 from time import sleep
 
 """
-
+Screen clearing function for Console-Execution (Bash/CMD)
+Aris Mandolini
+24.11.2020
 """
 def clearScreenConsole():
-    os.system("cls")
     if os.name == 'nt':     # for windows
         _ = os.system('cls')
     else:
         _ = os.system('clear')  # for mac and linux     (posix)
 
 
+"""
+Screen clearing function for PyCharm-Execution (Print lines without Bash/CMD)
+Aris Mandolini
+24.11.2020
+"""
 def clearScreenNormal():
-    print("\n"*100)
+    print("\n"*100)     # Prints 100 clear lines
 
 
+"""
+User-creating function for username storage and password.
+Aris Mandolini
+24.11.2020
+"""
 def registeredUser(username, password):
-    fileOpenNew = open("UserData/" + username.lower() + ".txt", "a")
-    fileOpenNew.write("Account correct.")
+    fileOpenNew = open("UserData/" + username.lower() + ".txt", "a")        # Opens (creates) new user file
+    fileOpenNew.write("Account correct.")       # Adds correction statement to user account
     fileOpenNew.write("\nPassword:")
-    fileOpenNew.write("\n" + password)
-    fileOpenNew.close()
+    fileOpenNew.write("\n" + password)      # Writes password in new line
+    fileOpenNew.close()     # Closes file
 
 
+"""
+Main login function, which is being called by other Modules. It handles login and registration.
+Aris Mandolini
+24.11.2020
+"""
 def loginFunction():
 
     loginLoop = 1
     while loginLoop == 1:
 
-        clearScreenNormal()
+        clearScreenNormal()     # Clears screen
         clearScreenConsole()
         print("Welcome to PPM v0.1")
         print("Please login. Write REGISTER in the Username field to create a new account.")
         print()
         username = input("Username: ")
-        if username.lower() == "register":
+        if username.lower() == "register":      # Checks if register option is selected
             registerLoop = 1
             while registerLoop == 1:
-                setUsername = input("Please select a new Username: ")
-                if os.path.isfile("UserData/" + setUsername.lower() + ".txt"):  # DO JURI
+                setUsername = input("Please select a new Username: ")       # Allocates new username
+                if os.path.isfile("UserData/" + setUsername.lower() + ".txt"):  # Checks if user (file) already exists
                     print("Username already in use.")
                     registerLoop = 1
                     sleep(5)
                 else:
                     passwordLoop = 1
                     while passwordLoop == 1:
-                        setPassword = input("Please select a new Password: ")
+                        setPassword = input("Please select a new Password: ")       # Allocates new password
                         confirmPassword = input("Please confirm Password: ")
-                        if setPassword == confirmPassword:
+                        if setPassword == confirmPassword:      # Checks if password correct
                             passwordLoop = 0
                         else:
                             print("Wrong password. Please try again.")
                             passwordLoop = 1
                             sleep(5)
-                    registeredUser(setUsername, setPassword)
+                    registeredUser(setUsername, setPassword)        # Calls registeredUser function
                     print("Username set up complete. You may now log in.")
                     registerLoop = 0
                     sleep(5)
 
             loginLoop = 1
         else:
-            if os.path.isfile("UserData/" + username.lower() + ".txt"):
-                fileOpenRead = open("UserData/" + username.lower() + ".txt", "r")
-                readFirstLine = fileOpenRead.readline()
-                readThirdLine = fileOpenRead.readlines()
-                if readFirstLine == "Account correct.\n":
-                    password = input("Password: ")
-                    if password == readThirdLine[1]:
+            if os.path.isfile("UserData/" + username.lower() + ".txt"):     # Checks if the user exists
+                fileOpenRead = open("UserData/" + username.lower() + ".txt", "r")       # Opens user file in read mode
+                readFirstLine = fileOpenRead.readline()     # Reads first line
+                readThirdLine = fileOpenRead.readlines()        # Reads third line
+                if readFirstLine == "Account correct.\n":       # Checks validity of user file
+                    password = input("Password: ")      # Allocates password
+                    if password == readThirdLine[1]:        # Checks validity of password
                         print("Welcome. Access Granted.")
                         sleep(5)
                     else:
@@ -87,8 +103,4 @@ def loginFunction():
                 sleep(5)
 
 
-loginFunction()
-
-"""
-Hoila des isch a kommentar.
-"""
+loginFunction()     # Calls loginFunction for testing purposes
