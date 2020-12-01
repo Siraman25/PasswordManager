7d69aa54839ea75916b12ed4acbe3a43ad931091
Siraman25 <mandoliniaris@gmail.com>
2020-12-01 11:52:47 +0100

    Organized files in a better way for Juri.

---
+++ GUI.py
@@ -0,0 +1,29 @@
+import sys
+
+"""
+Switch Case to open the subprograms
+Juri Ladurner
+30.11.2020
+"""
+def switch_Menue(argument):
+    switcher = {        # Switch-case function for python
+        1: "",                         # TODO; enter valid options in the cases
+        2: "",                         # TODO; enter valid options in the cases
+        3: sys.exit(0)
+    }
+
+
+"""
+The Menue where the user can decide with Subprograms he want to use
+Juri Ladurner
+30.11.2020
+"""
+def Menue():
+    possibility = 7
+    while possibility >=4 or possibility <= 0:                           # Loop if wrong input
+        print("Add new password: 1\nSearch password:  2\nQuit program:     3")
+        possibility = int(input("What do you want to do? "))
+    switch_Menue(possibility)                   # Call the switch_Menue function
+
+
+Menue()     # Calls Menue function for testing purposes
